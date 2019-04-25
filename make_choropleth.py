bab76627d90c133f478aa82a41f61879e40c3bf4
NaoY-2501 <solaris2501.fe2@gmail.com>
2019-04-25 15:14:47 +0900

    東京都のマーカーを追加

--- make_choropleth.py
+++ make_choropleth.py
@@ -14,17 +14,18 @@ base_df.dropna(subset=['prefecture'], inplace=True)
 grouped  = base_df.groupby('prefecture')
 
 pref_count = grouped.count().reset_index()
+pref_count['count'] = pref_count['month'].astype('int64')
 
 tokyo_idx = pref_count[pref_count['prefecture'] == '東京都'].index[0]
-pref_count.drop([tokyo_idx], inplace=True)
+choropleth_df = pref_count.drop([tokyo_idx])
 
 all = pref_count['month'].sum()
 
-pref_count['count'] = pref_count['month'].astype('int64')
-pref_count['rate'] = pref_count['count'].apply(lambda x: (x/all)*100)
-pref_count.drop(['month', 'title', 'address', 'lat', 'lon'], inplace=True, axis=1)
+# choropleth_df['count'] = choropleth_df['month'].astype('int64')
+choropleth_df['rate'] = choropleth_df['count'].apply(lambda x: (x/all)*100)
+choropleth_df.drop(['month', 'title', 'address', 'lat', 'lon'], inplace=True, axis=1)
 
-bins = list(pref_count['rate'].quantile([0, 0.25, 0.5, 0.75, 1]))
+bins = list(choropleth_df['rate'].quantile([0, 0.25, 0.5, 0.75, 1]))
 
 m = folium.Map(location=[35, 135], zoom_start=5)
 
@@ -33,7 +34,7 @@ m = folium.Map(location=[35, 135], zoom_start=5)
 folium.Choropleth(
     geo_data=geo,
     name='choropleth',
-    data=pref_count,
+    data=choropleth_df,
     columns=['prefecture', 'rate'],
     key_on='feature.properties.nam_ja',
     fill_color='YlGn',
@@ -43,6 +44,7 @@ folium.Choropleth(
     legend_name='東京開催イベントを除いた全体に対する開催数の割合(%)',
     highlight=True
 ).add_to(m)
+
 
 # add marker to map
 df = pd.read_csv('data/pref_center.csv')
